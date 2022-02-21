cab17da15e6c505d91ce775f8ee913a2866a186f
v.korobov <v.korobov@corp.mail.ru>
2022-02-21 19:39:50 +0300

    Action release

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import core from "@actions/core";
 import github from "@actions/github";
 import fetch from "node-fetch";
 
-import { parse, stringify } from 'yaml'
+import YAML from 'yaml'
 
 try {
     const apiUrl = core.getInput('api-url')
